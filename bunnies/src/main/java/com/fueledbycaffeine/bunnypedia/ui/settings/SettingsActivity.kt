3c6de23f16d99f58a87abccb27b11d58238fb4e0
Josh Friend <josh@fueledbycaffeine.com>
2018-01-14 20:04:41 -0500

    Add toolbar to settings

--- bunnies/src/main/java/com/fueledbycaffeine/bunnypedia/ui/settings/SettingsActivity.kt
+++ bunnies/src/main/java/com/fueledbycaffeine/bunnypedia/ui/settings/SettingsActivity.kt
@@ -3,11 +3,14 @@ package com.fueledbycaffeine.bunnypedia.ui.settings
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.fueledbycaffeine.bunnypedia.R
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity: AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_settings)
+    setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
     setTitle(R.string.settings)
   }
 }
