db255aef0ab11b0ff1682a0ab14a9d2ca4cf322e
Josh Friend <josh@fueledbycaffeine.com>
2018-01-27 12:23:42 -0500

    Add custom ul/li tag handler for pre-oreo

--- bunnies/src/main/java/com/fueledbycaffeine/bunnypedia/ext/android/TextView.kt
+++ bunnies/src/main/java/com/fueledbycaffeine/bunnypedia/ext/android/TextView.kt
@@ -3,12 +3,13 @@ package com.fueledbycaffeine.bunnypedia.ext.android
 import android.os.Build
 import android.text.Html
 import android.widget.TextView
+import com.fueledbycaffeine.bunnypedia.util.ListTagHandler
 
 fun TextView.setHtmlText(html: String) {
   if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
     text = Html.fromHtml(html, Html.FROM_HTML_MODE_COMPACT)
   } else {
     @Suppress("DEPRECATION")
-    text = Html.fromHtml(html)
+    text = Html.fromHtml(html, null, ListTagHandler())
   }
 }
