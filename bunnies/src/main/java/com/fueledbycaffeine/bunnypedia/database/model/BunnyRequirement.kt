d19c303aef074fb11343f91d7f3d144c85fc559e
Josh Friend <josh@fueledbycaffeine.com>
2018-10-03 23:26:14 -0400

    Indicate cards that require two bunnies
    closes #51

--- bunnies/src/main/java/com/fueledbycaffeine/bunnypedia/database/model/BunnyRequirement.kt
+++ bunnies/src/main/java/com/fueledbycaffeine/bunnypedia/database/model/BunnyRequirement.kt
@@ -7,12 +7,14 @@ enum class BunnyRequirement {
   NOT_APPLICABLE,
   NO,
   PLAY,
+  PLAY_X2,
   PLAY_AND_SAVE,
   ;
 
   val description: Int @StringRes get() {
     return when (this) {
       PLAY -> R.string.bunny_required_to_play
+      PLAY_X2 -> R.string.bunny_required_to_play_x2
       PLAY_AND_SAVE -> R.string.bunny_required_to_play_and_save
       else -> R.string.no
     }
