24a1b3cf22cc43dc4226ab32e9cb095f49e74724
Josh Friend <josh@fueledbycaffeine.com>
2019-04-21 22:16:18 -0400

    wip

--- database/dbseed.py
+++ database/dbseed.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 
+import re
+import os
 import json
 import glob
 import gzip
 import sqlite3
-import os
+import unicodedata
+from html.parser import HTMLParser
 from os.path import join, dirname, abspath
+
+class HTMLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs = True
+        self.fed = []
+
+    def handle_data(self, d):
+        self.fed.append(d)
+
+    def get_data(self):
+        return ''.join(self.fed)
 
 class _Table:
     @classmethod
@@ -72,6 +89,10 @@ class Card(_Table):
         self.psi = kwargs.get('psi')
         self.specialSeries = kwargs.get('specialSeries')
 
+    def localize(self, stringsfile):
+        # Add text to localized string registry and use the resource ID instead
+        self.title = stringsfile.add(self.title)
+
     def insert(self, conn):
         conn.execute(self.INSERT_STMT, self.__dict__)
 
@@ -103,6 +124,11 @@ class Rule(_Table):
         self.title = kwargs['title']
         self.text = kwargs['text']
 
+    def localize(self, stringsfile):
+        # Add text to localized string registry and use the resource ID instead
+        self.title = stringsfile.add(self.title)
+        self.text = stringsfile.add(self.text)
+
     def insert(self, conn):
         conn.execute(self.INSERT_STMT, self.__dict__)
 
@@ -114,18 +140,60 @@ class CardFts(_Table):
     """,
     """
     INSERT INTO CardFts(CardFts) VALUES ('rebuild')
+    """,
+    """
+    INSERT INTO CardFts(CardFts) VALUES ('optimize')
     """
     )
 
 
 ROOT_DIR = abspath(join(dirname(__file__), '..'))
 SRC_DIR = join(ROOT_DIR, 'bunnies/src/main')
+RES_DIR = join(SRC_DIR, 'res')
+VALUES_DIR = join(RES_DIR, 'values')
 ASSETS_DIR = join(SRC_DIR, 'assets')
 JSON_FILES = join(ROOT_DIR, 'database/*.json')
 DB_FILE = join(ROOT_DIR, 'database/db.sqlite3')
 DB_GZ_FOLDER = join(ASSETS_DIR, 'databases')
 DB_GZ_FILE = join(DB_GZ_FOLDER, 'db.sqlite3.gz')
+GENERATED_STRINGS_FILE = join(VALUES_DIR, 'db_strings.xml')
 
+_filename_ascii_strip_re = re.compile(r"[^A-Za-z0-9_]")
+_illegal_res_id_re = re.compile(r"^[0-9]")
+
+class StringsResXml:
+    _data = {}
+    _truncated_ids = {}
+
+    def _make_key(self, text, trunc=50):
+        nfkd_text = unicodedata.normalize('NFKD', base_text)
+        key = nfkd_text.encode('ascii', 'ignore').decode('ascii').lower()
+
+        for sep in os.path.sep, os.path.altsep:
+            if sep:
+                key = key.replace(sep, " ")
+                key = str(_filename_ascii_strip_re.sub("", "_".join(key.split()))).strip("._")
+        if _illegal_res_id_re.match(key):
+            # have to obey java identifier rules
+            key = f'n{key}'
+
+        if len(key) > trunc:
+            key = key[:trunc]
+
+    def add(self, base_text):
+        key = self._make_key(base_text)
+
+        self._data[key] = base_text
+        return key
+
+    def write(self, fp):
+        fp.write(b'<?xml version="1.0" encoding="utf-8"?>\n')
+        fp.write(b'<resources>\n')
+        for key, text in self._data.items():
+            fp.write('  <string name=\"{}\">{}</string>\n'.format(key, text).encode('utf-8'))
+        fp.write(b'</resources>\n')
+
+stringsfile = StringsResXml()
 cards_data = []
 for deck in glob.glob(JSON_FILES):
     with open(deck, 'rb') as fp:
@@ -151,15 +219,17 @@ conn.commit()
 
 for card_json in cards_data:
     try:
-        selectedCard = Card(**card_json)
-        selectedCard.insert(conn)
+        card = Card(**card_json)
+        card.localize(stringsfile)
+        card.insert(conn)
     except Exception as e:
         print("%s: %s" % (e.__class__.__name__, e.message))
         print("Card json: %s" % card_json)
         raise e
 
     for rule_json in card_json.get('rules', []):
-        rule = Rule(selectedCard.id, **rule_json)
+        rule = Rule(card.id, **rule_json)
+        rule.localize(stringsfile)
         rule.insert(conn)
 conn.commit()
 
@@ -170,6 +240,10 @@ conn.execute("pragma user_version = 1")
 conn.execute("pragma foreign_keys = 1")
 conn.commit()
 conn.close()
+
+# Export the localaized stringsfile
+with open(GENERATED_STRINGS_FILE, 'wb') as fp:
+    stringsfile.write(fp)
 
 try:
     os.makedirs(DB_GZ_FOLDER)
